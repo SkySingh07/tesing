b0004d46f7ea2e194940d427024bec4bb13d100f
Akash Kumar Singh <sakashbhu@gmail.com>
2026-02-18 13:19:34 +0530

    first commit

---
+++ a.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define lli long long
+#define vi vector<int>
+#define vii vector<vector<int>>
+#define pb push_back
+const lli mod = 1e9 + 7;
+void solve(){
+
+ 
+}
+signed main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);   cout.tie(0);
+    int t = 1;
+//	cin>>t;
+    while(t--)solve();
+    return 0; 
+}
